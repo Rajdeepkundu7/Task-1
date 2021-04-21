5175e8e1cc4f994e01074388a6d082af926b3748
Rajdeepkundu7 <82833866+Rajdeepkundu7@users.noreply.github.com>
2021-04-21 13:34:30 +0530

    Update task_1.R

--- task_1.R
+++ task_1.R
@@ -1,20 +1,32 @@
+#Task 1: Prediction Using Supervised ML
+#Author: Rajdeep Kundu
+
+
+library(MASS)                         #Calling the library 
+library(ggplot2)
 data=read.csv("scores.csv")           #Reading the given data
 data
-ndat=data.frame(x=9.25)            #For given value of study time
-x=data$Hours
-y=data$Scores                         #Defining the Variables 
-mod=lm(y~x)                         #Defining the linear model 
-summary(mod)                        #Summary of the linear model
-predict.lm(mod,ndat)                #Predicting the score value for the given study hour
-library(ggplot2)                    #Calling the library
+newdata=data.frame(x=9.25)            #Storing the given study hour
+x=data$Hours                          #Extracting the hours of study
+y=data$Scores                         #Extracting the scores 
+model=lm(y~x)                         #Fitting linear model y=ß0+ß1x+e 
+summary(model)                        #Details of the linear Regression fit
+predict.lm(model,newdata)             #Predicting the score value for the given study hour
 ggplot(data,aes(x,y)) + geom_point() +geom_smooth(method = "lm")+ labs(title = "Student Percentage vs Study Hours",
         x = "Study Hours",
-        y = "Student Percentage")    #Representing the Students' Scores vs Study Hours through a scatterplot 
-library(MASS)                        #Calling the library
-resi=stdres(mod)                     #Obtaining the Residual values from the linear model
+        y = "Student Percentage")    #Representing the Students' Scores vs Study Hours through a scatterplot                         
+resi=stdres(model)
+resi                   #Obtaining the Residual values from the linear model
 ggplot(data,aes(x, y = resi)) + geom_point() + geom_hline(yintercept=0)+ 
                                                           geom_hline(yintercept=-2, linetype="dashed", color = "red")+
                                                           geom_hline(yintercept=2, linetype="dashed", color = "red")
                                                           labs(title = "Student Percentage vs Study Hours",
                                                                                 x = "Fitted Values",
-                                                                                y = "Residuals")        #Representing the Residual values vs Fitted Values of the linear model through a Scatterplot
+                                                                                y = "Residuals")
+
+                                     #Representing the Residual Values vs Fitted Values of the linear model through Scatterplot
+
+#Conclusion
+#For 9.25 hours of study hours, predicted score is 92.91
+#Standardized Residuals are mostly Randomly Scattered.The explanatory variable explains the variability of dependent variable well.
+#There is only one point to suspect for being influencial point
